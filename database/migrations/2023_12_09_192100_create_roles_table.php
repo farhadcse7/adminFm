fe67b22dd2e6090400439fdc5b4d30e918187ecf
farhadcse7 <fmfarhad23@gmail.com>
2023-12-10 01:56:20 +0600

    Module role permission page model and migration create

--- database/migrations/2023_12_09_192100_create_roles_table.php
+++ database/migrations/2023_12_09_192100_create_roles_table.php
@@ -11,14 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('role_id')->constrained('roles');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('role_name');
+            $table->string('role_slug');
+            $table->text('role_note')->nullable()->comment('describes the role');
+            $table->boolean('is_deleteable')->default(true);
             $table->timestamps();
             $table->softDeletes();
         });
@@ -29,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('roles');
     }
 };
